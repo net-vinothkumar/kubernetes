5afedba908c5fbd53683ecdc7c7d82d5d9034c19
vinothkumar <net.vinothkumar@gmail.com>
2020-02-03 15:07:34 +0100

    k8s config files

--- src/test/java/com/kubernetes/demo/K8SDemoApplicationTests.java
+++ src/test/java/com/kubernetes/demo/K8SDemoApplicationTests.java
@@ -5,12 +5,18 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+
 public class K8SDemoApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testme () {
+		Stream<Integer> stream = Stream.of(1,11,3,15,4,25);
+		int result  = stream.filter(i -> i > 10).mapToInt(i -> i).sum();
+		System.out.println(result);
 	}
 
 }
